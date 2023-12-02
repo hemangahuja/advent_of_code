e1930e92eeb26f2e103ad001572254f37b6a0cb2
hemangahuja <hemangahujaeotw@gmail.com>
2023-12-02 13:49:52 +0530

    day2 part2

--- day2.js
+++ day2.js
@@ -11,6 +11,7 @@ function solve(input) {
         ["red", 12]
     ]
     let answer = 0;
+    let answer2 = 0;
     games.forEach(game => {
         const id = game[0].split(" ").at(1);
         const sets = game[1].trim().split("; ").map(x => x.split(", ").map(x => x.split(" "))).map(x => Object.fromEntries(x.map(y => y.reverse())));
@@ -18,6 +19,13 @@ function solve(input) {
         if (sets.every(set => possible.every(([color, value]) => !(color in set) || Number(set[color]) <= value))) {
             answer += Number(id);
         }
+        const max = new Map();
+        sets.forEach(set => Object.entries(set).forEach(([color, value]) => {
+            max.set(color, Math.max(max.get(color) ?? 0, value));
+        }))
+        answer2 += [...max.values()].reduce((p, c) => p * c, 1);
     })
-    console.log(answer);
+    console.log(answer, answer2);
+
 }
+
